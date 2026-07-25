568403367812812330dd1d6f15e8073d3bf40daf
Ismael Gallegos <fcgg241007@outlook.com>
2026-07-25 14:23:58 -0700

    Contiene la lógica de la calculadora, seleccionando elementos del DOM, declarando 3 variables que almacenan el esyado de la operación; cada botón tiene un evento click que evalua el tipos de valor presionado.

---
+++ js/app.js
@@ -0,0 +1,62 @@
+/*========================
+Elementos del DOM
+========================= */
+
+const pantalla = document.getElementById("pantalla");
+const botones = document.querySelectorAll ("btn");    
+
+/*Variables para almacenar la operación */
+let numeroAnterior = "";
+let operador = "";
+let operacionActual = "";
+
+/*========================
+Asignación de eventos a todos los botones
+========================= */
+
+botones.forEach(boton => {
+    boton.addEventListener("click", () => {
+        const valor = boton.textContent;
+
+        /* Si el boton es un número */
+        if (!isNan(valor)) {
+            operacionActual += valor;
+            pantalla.value = operacionActual;
+        }
+
+        /* Si es un operador */
+        else if (valor === "+" || valor === "-" || valor === "*" || valor === "/") {
+            numeroAnterior = operacionActual;             // Guardar el número actual como número anterior
+            operador = valor;                            // Guardar el operador
+            operacionActual = "";                        // Limpiar la operación actual
+        }
+
+        /* Si es el botón de igual (=) */
+        else if (valor === "=") {
+            if (nuemroAnterior !== "" && operador !== "" && operacionActual !== "") {
+                const num1 = parseFloat(numeroAnterior);
+                const num2 = parseFloat(operacionActual);
+                let resultado;
+
+                switch (operador) {
+                    case "+": resultado = num1 + num2; break;
+                    case "-": resultado = num1 - num2; break;
+                    case "*": resultado = num1 * num2; break;
+                    case "/": resultado = num2 !== 0 ? num1 / num2 : "Error"; break;
+                }
+                pantalla.value = resultado;
+                operacionActual = resultado.toString(); // para permitir operaciones consecutivas
+            }
+        }
+
+        /* Si es el botón de borrar (C) */
+        else if (valor === "C") {
+            pantalla.value = "";
+            numeroAnterior = "";
+            operador = "";
+            operacionActual = "";
+        }
+    });
+});
+
+
